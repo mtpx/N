b23db0a915deb2af5d0f1ca5e7017d2f09c9a0eb
Mateusz Krasny <mkrasny@opidomain.local>
2019-07-05 13:39:34 +0200

    first commit

--- src/main/java/pages/Login.java
+++ src/main/java/pages/Login.java
@@ -1,10 +1,8 @@
 package pages;
-
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
 import io.qameta.allure.Step;
 import org.openqa.selenium.By;
-
 import java.util.List;
 
 import static com.codeborne.selenide.Selenide.$;
@@ -12,31 +10,31 @@ import static com.codeborne.selenide.Selenide.$$;
 
 public class Login extends _TestBase {
 
-    private static SelenideElement email = $("#email"),
-            password = $("#password"),
+    private static SelenideElement emailInput = $("#email"),
+            passwordInput = $("#password"),
             loginButton = $("#loginButton"),
             registerButton = $("##j_idt60"),
-            rememberMeCheckbox = $("#remember-me");
-    private static List<SelenideElement> errorMessages = $$(By.xpath("//div[@id='messages_container']//span[@class='ui-growl-title']"));
+            rememberMeCheckbox = $("#remember-me"),
+            popupAboutLogin = $(".ui-growl-title");
 
     @Step("Logowanie za pomoca: user: {1}, pass: {2}")
     public void login(String login, String password) throws InterruptedException {
-        $(this.email).clear();
-        $(this.password).clear();
-        $(this.email).setValue(login);
-        $(this.password).setValue(password);
+        $(this.emailInput).clear();
+        $(this.passwordInput).clear();
+        $(this.emailInput).setValue(login);
+        $(this.passwordInput).setValue(password);
         Thread.sleep(200);
         $(this.loginButton).shouldBe(Condition.enabled).click();
     }
 
     @Step("Pobieranie wiadomosci z bledem logowania")
     public SelenideElement getErrorMessage(){
-        return errorMessages.get(0);
+        return popupAboutLogin;
     }
 
-    @Step("Pobieranie drugiej wiadomosci z bledem logowania")
-    public SelenideElement getSecondErrorMessage(){
-        return errorMessages.get(1);
+    public void waitForPopupAboutLogin(){
+        popupAboutLogin.shouldBe(Condition.disappear);
     }
+
 
 }
--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -11,7 +11,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 
-import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.open;
 import static utils.Utils.generateRandomString;
 
@@ -24,6 +23,7 @@ public class Tests {
         Configuration.holdBrowserOpen = false;
         Configuration.browser = "chrome";
         Configuration.timeout = 10000;
+        Configuration.headless = true;
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(true));
     }
 
@@ -42,120 +42,11 @@ public class Tests {
         open(Constants.testurl);
         //getWebDriver().manage().window().maximize();
 
-        new _TestBase().clickLogin();
+       // new _TestBase().clickLogin();
 
-        log.info("2. Logowanie bez danych");
+        log.info("2. Logowanie");
         Login objLogin = new Login();
-        objLogin.login("", "");
-        objLogin.getErrorMessage().shouldHave(text("Pole Email jest wymagane."));
-        objLogin.getSecondErrorMessage().shouldHave(text("Pole Hasło jest wymagane."));
-
-        log.info("3. Poprawne logowanie");
-
-        objLogin.login("matpanx@gmail.com", "Bmxheni@1");
-
-    }
-
-    @Test(enabled = true, priority = 1)
-    @Description("Wypelnianie wniosku Eksperckiego")
-    //@Parameters({ "name", "phone", "password", "001regFeedback", "blankValidationError" })
-    public void test002_fillExpertsApplication(/*String name, String password, String password*/) throws Exception {
-
-        log.info("1. Przejście do programow");
-        HomePage objHomePage = new HomePage();
-        objHomePage.deleteExistingApplications();
-        objHomePage.clickPrograms();
-
-        log.info("2. przejscie do expert application");
-        Applications objApplications = new Applications();
-        objApplications.clickFillExpertsApplication();
-
-
-        ExpertsApplication objExpertsApplication = new ExpertsApplication();
-        objExpertsApplication.clickPolishLanguageCheckbox();
-        objExpertsApplication.clickSubmitApplicationButton();
-        objExpertsApplication.clickSavingDraftTitleCloseButton();
-        Assert.assertEquals(objExpertsApplication.getValidationErrorDialogBoxLabelText(), "Występują błędy walidacji. Proszę sprawdzić, czy wszystkie pola zostały wypełnione poprawnie.");
-        objExpertsApplication.clickOkOnValidationErrorDialogBox();
-
-        Assert.assertEquals(objExpertsApplication.getBottomErrorHeaderText(), "Twój formularz zawiera 1 lub więcej błędów");
-        objExpertsApplication.clickInterestedInNawaCheckbox();
-        objExpertsApplication.selectRandomNawaProgram();
-        // objExpertsApplication.selectSecondRandomNawaProgram();
-
-        objExpertsApplication.setAcademicTitle(faker.streetSuffix());
-        objExpertsApplication.setPhoneNumber("123456987");
-        objExpertsApplication.selectRandomThematicScopeProgram();
-        objExpertsApplication.selectRandomEnglishLevel();
-        objExpertsApplication.clickRequiredCheckboxes();
-
-        log.info("3. Zapisywanie kopii roboczej");
-        objExpertsApplication.clickSaveAsCopyButton();
-        objExpertsApplication.clickMyApplications();
-
-        log.info("4. Usuwanie formularza");
-        objHomePage.clickDeleteFirstApplication();
-        objHomePage.clickConfirmDeleteApplication();
-    }
-
-
-
-    @Test (enabled=true, priority=1)
-    //@Parameters({ "name", "phone", "password", "001regFeedback", "blankValidationError" })
-    public void test003_fillPolishLanguagePromotion() throws Exception {
-
-
-        log.info("1. Przejście do programow");
-        HomePage objHomePage = new HomePage();
-        //objHomePage.deleteExistingApplications();
-        objHomePage.clickPrograms();
-        objHomePage.acceptAlertIfPresent();
-
-        log.info("2. Wypełanie wniosku promocja jezyka polskiego");
-        Applications objApplications = new Applications();
-        objApplications.clickFillPolishLanguagePromotion();
-
-        log.info("3. Wypełanie metryki wniosku");
-        PolishLanguagePromotion objPolishLanguagePromotion = new PolishLanguagePromotion();
-        objPolishLanguagePromotion.setProjectRealizationPlace(faker.streetSuffix());
-        objPolishLanguagePromotion.clickNextPageOfApplication();
-
-        log.info("4. Wypełanie drugiej strony wniosku");
-        objPolishLanguagePromotion.selectRandomUnitStatus();
-        objPolishLanguagePromotion.selectRandomUnitFullName();
-        objPolishLanguagePromotion.setNip("1165830299");
-        objPolishLanguagePromotion.setUnitForm(faker.streetSuffix());
-        objPolishLanguagePromotion.setDetailsOfPersonSubmittingApplication(faker.country(), "794350654");
-        objPolishLanguagePromotion.selectRandomAuthorizationToSendApplication();
-        objPolishLanguagePromotion.setContactPerson(faker.firstName(), faker.lastName(), faker.country(),"xxx@asd.pl","794350654");
-        objPolishLanguagePromotion.setFinanceContactPerson(faker.firstName(), faker.lastName(), faker.country(),"xxx@asd.pl","794350654");
-        objPolishLanguagePromotion.setDescriptionOfApplicant(generateRandomString(1010));
-        objPolishLanguagePromotion.setExperienceOfApplicant(generateRandomString(1010));
-
-        log.info("5. Wypełanie trzeciej strony wniosku");
-        objPolishLanguagePromotion.clickNextPageOfApplication();
-
-        log.info("5. Wypełanie czwartej strony wniosku");
-        objPolishLanguagePromotion.clickNextPageOfApplication();
-        objPolishLanguagePromotion.setProjectTitle(generateRandomString(15));
-        objPolishLanguagePromotion.setProjectStartDate("2019-10-09");
-        objPolishLanguagePromotion.setProjectEndDate("2020-02-09");
-        objPolishLanguagePromotion.setShortProjectDescription(generateRandomString(1010));
-        objPolishLanguagePromotion.setProjectAnalysis(generateRandomString(1010));
-        objPolishLanguagePromotion.setProjectInnovation(generateRandomString(1010));
-        objPolishLanguagePromotion.setTargetGroupDescription(generateRandomString(1010));
-        objPolishLanguagePromotion.setProjectResults(generateRandomString(1010));
-        objPolishLanguagePromotion.setProjectSchedule(generateRandomString(1010));
-        objPolishLanguagePromotion.setProjectManagement(generateRandomString(15));
-        objPolishLanguagePromotion.setProjectEquality(generateRandomString(15));
-        objPolishLanguagePromotion.setProjectMonitoring(generateRandomString(15));
-        objPolishLanguagePromotion.setProjectEvaluation(generateRandomString(15));
-        objPolishLanguagePromotion.setRiskDescription(generateRandomString(15));
-        objPolishLanguagePromotion.selectRandomRiskProbability();
-        objPolishLanguagePromotion.selectRandomRiskImpact();
-        objPolishLanguagePromotion.setRiskPrevention(generateRandomString(15));
-        objPolishLanguagePromotion.setRiskImpactMinimalization(generateRandomString(15));
-
+        objLogin.login("mkrasny@opi.org.pl", "Wniosek1!");
 
     }
 
